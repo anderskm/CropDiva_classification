9592add8ff9da619bd456799ee6bcbb86aa9d3e8
Anders Krogh Mortensen <au234627@uni.au.dk>
2023-09-21 15:03:09 +0200

    Add label dict to filtering

--- main_1_filter_dataframe.py
+++ main_1_filter_dataframe.py
@@ -14,6 +14,7 @@ def main():
     parser.add_argument('--input_df', action='store', default='dataframe_annotations__all.pkl', type=str, help='Filename of pickle file containing dataframe to be filtered (default: %(default)s).')
     parser.add_argument('--output_df', action='store', default='', type=str, help='Filename of filtered dataframe. Leave empty to simply append "__filtered" to the filename (default: %(default)s).')
     parser.add_argument('--discard_labels_file', action='store', default='', type=str, help='Filename of json file with list of classes to discard. Leave empty to remove classes with less than 100 samples (default: %(default)s).')
+    parser.add_argument('--labels_dict', action='store', default='', type=str, help='Filename of json file with dictionany of classes to keep. Leave empty to remove classes with less than 100 samples (default: %(default)s).')
 
     args = vars(parser.parse_known_args()[0])
     print('\nArguments: ', args)
@@ -23,11 +24,12 @@ def main():
     output_dataframe_filename = args['output_df']
     basename, ext = os.path.splitext(os.path.basename(input_dataframe_filename))
     if not output_dataframe_filename:
-        output_dataframe_filename = os.path.join(os.path.dirname(input_dataframe_filename), basename + '__filtered' + ext)
+        output_dataframe_filename = os.path.join(os.path.dirname(input_dataframe_filename), basename + '__filtered3' + ext)
     print('Output: ', output_dataframe_filename)
     labels_discard_file = args['discard_labels_file']
     if not labels_discard_file:
         labels_discard_file = os.path.join(os.path.dirname(input_dataframe_filename), basename + '__labels_discard.json')
+    labels_dict_file = args['labels_dict']
     
     df_all = pd.read_pickle(input_dataframe_filename)
 
@@ -38,24 +40,35 @@ def main():
 
     print('\n\n### Cleaning up dataset ###')
 
-    # Remove images from classes with few samples
-    print('\nRemoving classes with too few samples')
+    print('\n# Filtering classes #')
 
-    if os.path.exists(labels_discard_file):
-        with open(labels_discard_file,'r') as fob:
-            labels_discard = json.load(fob)
+    if labels_dict_file:
+        with open(labels_dict_file,'r') as fob:
+                labels_dict = json.load(fob)
+        print(labels_dict)
+        df_filt_list = []
+        for label in labels_dict:
+            df_filt_list.append(utils.dataframe_filtering(df_all, df_all['label'] == label)[0])
+        df_filt = pd.concat(df_filt_list)
     else:
-        df_label_count = df_all.groupby(['label'])['image'].count()
+        # Remove images from classes with few samples
+        print('\nRemoving classes with too few samples')
+
+        if os.path.exists(labels_discard_file):
+            with open(labels_discard_file,'r') as fob:
+                labels_discard = json.load(fob)
+        else:
+            df_label_count = df_all.groupby(['label'])['image'].count()
+            
+            labels = df_label_count.index.to_list()
+            label_count = df_label_count.to_list()
+            labels_discard = [l for l,c in zip(labels, label_count) if c < 100]
+            with open(labels_discard_file,'w') as fob:
+                json.dump(labels_discard, fob)
         
-        labels = df_label_count.index.to_list()
-        label_count = df_label_count.to_list()
-        labels_discard = [l for l,c in zip(labels, label_count) if c < 100]
-        with open(labels_discard_file,'w') as fob:
-            json.dump(labels_discard, fob)
-    
-    df_filt = df_all
-    for label in labels_discard:
-        df_filt, _ = utils.dataframe_filtering(df_filt, df_filt['label'] != label)
+        df_filt = df_all
+        for label in labels_discard:
+            df_filt, _ = utils.dataframe_filtering(df_filt, df_filt['label'] != label)
 
     print('\n### End of Cleaning up dataset ###\n\n')
 
