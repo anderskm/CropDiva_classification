6580e0956422afd40d6035624ed6018e0e3a9126
Anders Krogh Mortensen <au234627@uni.au.dk>
2024-01-10 21:28:16 +0100

    Add commandline args. Add comments. Remove unused code.

--- main_0_make_dataframe.py
+++ main_0_make_dataframe.py
@@ -1,18 +1,11 @@
+import argparse
 import glob
-# import numpy as np
 import os
 import pandas as pd
 from PIL import Image
 import tqdm
-# import warnings
 
 import utils
-
-# warnings.simplefilter('always')
-
-output_dataframe_filename = 'dataframe_annotations__all.pkl'
-
-data_folder = 'C:/Projekter/2023_CropDiva__Weed_classificaiton/TrainingData/05SEP23_raw'
 
 def get_subfolders(main_folder):
     main_folder_content = os.listdir (main_folder)
@@ -23,51 +16,78 @@ def get_subfolders(main_folder):
             subfolders.append(obj)
     return subfolders
 
-image_folders = get_subfolders(data_folder)
+def main():
+    # Setup input argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output_df', action='store', default='dataframe_annotations__all.pkl', type=str, help='Filename of pickle file containing dataframe. (default: %(default)s).')
+    parser.add_argument('--data_folder', action='store', default='', type=str, help='Path to folder containing the image data, where each subfolder corresponds to a specific class (default: %(default)s).')
+    parser.add_argument('--img_ext', action='store', default='png', type=str, help='File extention of images. (default: %(default)s).')
 
-DFs = []
+    # Parse inputs
+    args = vars(parser.parse_known_args()[0])
+    print('\nArguments: ', args)
+    data_folder = args['data_folder']
+    print('Input data folder: ', data_folder)
+    img_ext = args['img_ext']
+    print('Image extension: ', img_ext)
+    output_dataframe_filename = args['output_df']
+    print('Output file: ', output_dataframe_filename)
 
-# Load annotations
-for img_folder in tqdm.tqdm(image_folders, desc='Parsing subfolders'):
-    image_list = glob.glob(os.path.join(data_folder, img_folder,'*.png'))
+    # Get all subfolders in main data folder
+    image_folders = get_subfolders(data_folder)
 
-    df_bounding_boxes_list = []
-    # df_bounding_boxes = pd.DataFrame(columns=['image','folder','EPPO','UploadID','ImageID','BBoxID','width','height','area','label'])
-    df_bounding_boxes_list.append(pd.DataFrame(columns=['image','folder','EPPO','UploadID','ImageID','BBoxID','width','height','area','label']))
-    for image_path in tqdm.tqdm(image_list, desc='Parsing ' + img_folder, leave=False):
-        folder, image_name = os.path.split(image_path)
+    # Loop through classes/subfolders
+    DFs = []
+    for img_folder in tqdm.tqdm(image_folders, desc='Parsing subfolders'):
+        # Find all images in subfolder
+        image_list = glob.glob(os.path.join(data_folder, img_folder,'*.' + img_ext))
 
-        image_name_parts = os.path.splitext(image_name)[0].split('_')
-        EPPO = image_name_parts[0]
-        UploadID = image_name_parts[1]
-        ImageID = image_name_parts[2]
-        BBoxID = image_name_parts[3]
+        # Initialize dataframe for class subfolder
+        df_bounding_boxes_list = []
+        df_bounding_boxes_list.append(pd.DataFrame(columns=['image','folder','EPPO','UploadID','ImageID','BBoxID','width','height','area','label']))
+        for image_path in tqdm.tqdm(image_list, desc='Parsing ' + img_folder, leave=False):
+            folder, image_name = os.path.split(image_path)
+            
+            # Extract meta data from filename
+            image_name_parts = os.path.splitext(image_name)[0].split('_')
+            EPPO = image_name_parts[0]
+            UploadID = image_name_parts[1]
+            ImageID = image_name_parts[2]
+            BBoxID = image_name_parts[3]
 
-        # Load image to get size
-        with Image.open(image_path) as im:
-            width, height = im.size
-            area = width*height
+            # Load image to get size
+            with Image.open(image_path) as im:
+                width, height = im.size
+                area = width*height
 
-        label = img_folder
+            label = img_folder
+            # Store image meta data as a row in a data frame
+            df_bounding_boxes_list.append(pd.DataFrame.from_dict({'image': [image_name],
+                                                                    'folder': [img_folder],
+                                                                    'EPPO': [EPPO],
+                                                                    'UploadID': [UploadID],
+                                                                    'ImageID': [ImageID],
+                                                                    'BBoxID': [BBoxID],
+                                                                    'width': [width],
+                                                                    'height': [height],
+                                                                    'area': [area],
+                                                                    'label': [label]
+                                                                }))
+        # Concatenate the individual image dataframes into a single dataframe for the class
+        df_bounding_boxes = pd.concat(df_bounding_boxes_list)
+        DFs.append(df_bounding_boxes)
+    
+    # Concatenate the dataframes from each subfolder into a single dataframe containing all classes
+    df_all = pd.concat(DFs, ignore_index=True)
+    
+    # Save as pickle file
+    df_all.to_pickle(output_dataframe_filename)
+    print('Dataframe saved to: ', output_dataframe_filename)
 
-        df_bounding_boxes_list.append(pd.DataFrame.from_dict({'image': [image_name],
-                                                                'folder': [img_folder],
-                                                                'EPPO': [EPPO],
-                                                                'UploadID': [UploadID],
-                                                                'ImageID': [ImageID],
-                                                                'BBoxID': [BBoxID],
-                                                                'width': [width],
-                                                                'height': [height],
-                                                                'area': [area],
-                                                                'label': [label]
-                                                            }))
-    df_bounding_boxes = pd.concat(df_bounding_boxes_list)
-    DFs.append(df_bounding_boxes)
+    # Print a summary
+    utils.print_annotation_stats(df_all)
 
-df_all = pd.concat(DFs, ignore_index=True)
+    print('done')
 
-df_all.to_pickle(output_dataframe_filename)
-
-utils.print_annotation_stats(df_all)
-
-print('done')
+if __name__ == '__main__':
+    main()
